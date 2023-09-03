cde232adda0adc19d41facc700357a2bda1c98bc
Denzel Gitonga <gitongadenzel@gmail.com>
2023-09-03 16:29:46 +0300

    remove background

--- rm_bg.py
+++ rm_bg.py
@@ -1,5 +1,7 @@
 from rembg import remove
 from PIL import Image
+import tkinter as tk
+from tkinter import filedialog
 
 def remove_background(input_path, output_path):
     try:
@@ -21,21 +23,23 @@ def remove_background(input_path, output_path):
     except Exception as e:
         print(f"An error occurred: {e}")
 
+def select_input_image():
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename(title="Select the image to remove background")
+    return file_path
+
+def select_output_path():
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.asksaveasfilename(title="Select where to save the new image")
+    return file_path
+
 if __name__ == "__main__":
-    input_path = input("Enter the path to the input image: ")
-    output_path = input("Enter the path to save the output image: ")
+    input_path = select_input_image()
+    output_path = select_output_path()
     
-    remove_background(input_path, output_path)
-
-
-
-# input_path = "D:\gtest.png"
-# output_path = "D:\output.png"  # Save as PNG
-
-# img_process = Image.open(input_path).convert("RGBA")  # Convert to RGBA
-# output = remove(img_process)
-# output.save(output_path)
-
-# # Close the images
-# img_process.close()
-# output.close()
+    if input_path and output_path:
+        remove_background(input_path, output_path)
+    else:
+        print("Operation canceled.")
